6d31e0b302c64bee5872456a57aa96ca0e596983
INOCCENT-Dev <105031200+INOCCENT-Dev@users.noreply.github.com>
2022-05-15 00:08:40 +0900

    Add files via upload

--- code.js
+++ code.js
@@ -9,3 +9,22 @@ window.onload = function() {
   };
   img.src = "img/garden.jpg"
 };
+
+function drawPixel(ctx,data){
+  ctx.beginPath();
+  for(let x = 0; x < data.width / 20; x ++){
+    for(let y = 0; y < data.height / 20; y ++){
+      ctx.fillStle = getRGB(x*10,y*10,data);
+      ctx.arc(x*10,y*10,10,0,2*Math.PI,true);
+      ctx.fill();
+    }
+  }
+}
+
+function getRGB(x,y,data){
+  let R = data[imageData.width * 4 * y + 4 * x];
+  let G = data[imageData.width * 4 * y + 4 * x + 1];
+  let B = data[imageData.width * 4 * y + 4 * x + 2];
+  let rgb = "rgb(" + R + "," + G + "," + B + ")"
+  return rgb;
+}
