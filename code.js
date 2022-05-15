494ed7552459d88874ddda6fbe644f53a464ea5e
INOCCENT-Dev <105031200+INOCCENT-Dev@users.noreply.github.com>
2022-05-15 15:38:26 +0900

    Add files via upload

--- code.js
+++ code.js
@@ -6,26 +6,27 @@ window.onload = function() {
     ctx.drawImage(img,0,0,1200,600);
     let imageData = ctx.getImageData(0,0,canvas.width,canvas.height);
     
-    setInterval(function(){
+    setTimeout(function(){
       drawPixel(ctx,imageData);
-    },33);
+    },500);
   };
-  img.src = "img/garden.jpg"
+  img.src = "img/castle.jpg"
 };
 
 function drawPixel(ctx,data){
-  
-
-  ctx.fillStyle = 'rgb(255,255,255)';
-  ctx.fillRect(0,0,data.width,data.height);
 
   for(let x = 0; x < data.width / 20; x ++){
-    for(let y = 0; y < data.height / 20; y ++){
-      ctx.beginPath();
-      ctx.fillStyle = getRGB(x*20+10,y*20+10,data);
-      ctx.arc(x*20+10,y*20+10,10,0,2*Math.PI,true);
-      ctx.fill();
-    }
+    setTimeout(function(){
+      ctx.fillStyle = 'rgb(255,255,255)';
+      ctx.fillRect(0,0,(x+1)*20,data.height);
+
+      for(let y = 0; y < data.height / 20; y ++){
+        ctx.beginPath();
+        ctx.fillStyle = getRGB(x*20+10,y*20+10,data);
+        ctx.arc(x*20+10,y*20+10,10,0,2*Math.PI,true);
+        ctx.fill();
+      }
+    },100);
   }
 }
 
