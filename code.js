4fd17254740ef47ec557ecb096b70e5c6e45f8f2
INOCCENT-Dev <105031200+INOCCENT-Dev@users.noreply.github.com>
2022-05-16 18:42:44 +0900

    Add files via upload

--- code.js
+++ code.js
@@ -1,84 +1,42 @@
-  window.onload = function() {
-    const canvas = document.getElementById("mycanvas"); 
-    const ctx = canvas.getContext("2d");
-    const TIME_INTERVAL = 33; 
-    const SPOT_NUMBER = 100;
+window.onload = function() {
+  const canvas = document.getElementById("mycanvas");
+  const ctx = canvas.getContext("2d");
+  const img = new Image();
+  img.onload = function(){
+    ctx.drawImage(img,0,0,canvas.width,canvas.height);
+    let imageData = ctx.getImageData(0,0,canvas.width,canvas.height);
+    // console.log(imageData.__proto__);
+    setTimeout(function(){
+      drawPixel(ctx,imageData);
+    },500);
+  };
+  img.src = "img/castle.jpg"
+};
 
-    let spot1 = []; // wave 1 spots
-    let spot2 = []; // wave 2 spots
-    let spot3 = []; // wave 3 spots
-
-    let color = ['rgba(255,100,165,0.5)','rgba(165,255,100,0.5)','rgba(100,165,255,0.5)'];
-
-
-    for(let i = 0 ; i < SPOT_NUMBER; i ++){ // reset spot
-      let x = (canvas.width / (SPOT_NUMBER-1)) * i;
-      
-      spot1[i] = new Spot(x,200,i);
-      spot2[i] = new Spot(x,200,i+20);
-      spot3[i] = new Spot(x,200,i+40);
-    }
-
-    window.onclick = function() {
-      for(let i = 0; i < 3; i ++){
-        color[i] = setColorAsRandom(50,255);
-      }
-    }
-    
-    setInterval(function(){
+function drawPixel(ctx,data){
+  for(let x = 10; x < data.width; x += 20){
+    setTimeout(function(){
       ctx.fillStyle = 'rgb(255,255,255)';
-      ctx.fillRect(0,0,canvas.width,canvas.height);
+      ctx.fillRect(x-10,0,x+10,data.height);
 
-      drawDraw(spot1,color[0]);
-      drawDraw(spot2,color[1]);
-      drawDraw(spot3,color[2]);
-    },TIME_INTERVAL);
-
-    function drawDraw(spot,color){ // draw wave function
-      let gapY = spot[0].setGapY(SPOT_NUMBER);
-      ctx.fillStyle= color;
-      ctx.beginPath();
-      ctx.moveTo(0,200);
-
-      for(let i = 0 ; i < SPOT_NUMBER; i ++){  
-        spot[i].setPosition(SPOT_NUMBER,gapY).wave().draw(ctx,i);
+      for(let y = 10; y < data.height; y += 20){
+        ctx.beginPath();
+        ctx.fillStyle = getRGB(x,y,data);
+        // ctx.font = "19px Arial";
+        // ctx.textAlign = "center";
+        // ctx.fillText("A", x,y);
+        ctx.arc(x,y,10,0,2*Math.PI,true);
+        ctx.fill();
       }
-      ctx.arc(300,200,300,0,Math.PI,false);
-      ctx.fill();
-    }
-  };
-
-  function setColorAsRandom(lmin,lmax){
-    let R = Math.floor(lmin+Math.random()*(lmax - lmin));
-    let G = Math.floor(lmin+Math.random()*(lmax - lmin));
-    let B = Math.floor(lmin+Math.random()*(lmax - lmin));
-    return 'rgba('+R+','+B+','+G+',0.5)'; 
+    },1000);
   }
+}
 
-  function Spot(x,y,tmpX){
-    this.x = x;
-    this.y = y;
-    this.tmpX = tmpX;
-  }
+function getRGB(x,y,data){
+  let R = data.data[data.width * 4 * y + 4 * x];
+  let G = data.data[data.width * 4 * y + 4 * x + 1];
+  let B = data.data[data.width * 4 * y + 4 * x + 2];
+  let rgb = "rgb(" + R + "," + G + "," + B + ")";
+  return rgb;
+}
 
-  Spot.prototype = {
-    draw: function(ctx){ // draw wave line
-      ctx.lineTo(this.x,this.y);
-      return this;
-    },
-    wave: function(){ // make wave 
-      this.tmpX ++;
-      return this;
-    },
-    setPosition: function(num,gap){ // set y position
-      if(this.tmpX == 0){
-        this.y = 200;
-      }else{
-        this.y = gap + Math.sin(Math.PI*2/num*this.tmpX) * 40;
-      }
-      return this;
-    },
-    setGapY: function(num){ 
-      return 200 - Math.sin(Math.PI*2/num*this.tmpX) * 40;
-    }
-  };
