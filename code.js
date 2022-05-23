32943cc4917346eb1b66d051517cab700899996c
INOCCENT-Dev <105031200+INOCCENT-Dev@users.noreply.github.com>
2022-05-24 08:00:41 +0900

    Add files via upload

--- code.js
+++ code.js
@@ -7,7 +7,7 @@ window.onload = function() {
   let isZoom = false;
 
   img.onload = function(){
-    ctx.drawImage(img,0,0,canvas.width,canvas.height);
+    drawImg();
 
     canvas.addEventListener('click',function(event){
       if(isZoom){
