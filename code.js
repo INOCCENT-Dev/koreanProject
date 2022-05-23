41723c00e7b40bedcb060735c86d309abb87e0b6
INOCCENT-Dev <105031200+INOCCENT-Dev@users.noreply.github.com>
2022-05-24 00:44:04 +0900

    Add files via upload

--- code.js
+++ code.js
@@ -2,33 +2,39 @@ window.onload = function() {
   const canvas = document.getElementById("mycanvas");
   const ctx = canvas.getContext("2d");
   const img = new Image();
+  //canvas.style.width = window.width +'';
+  //canvas.style.height = window.height +'';
   img.onload = function(){
     ctx.drawImage(img,0,0,canvas.width,canvas.height);
     let imageData = ctx.getImageData(0,0,canvas.width,canvas.height);
-    // console.log(imageData.__proto__);
-    setTimeout(function(){
+
+    canvas.addEventListener('click',function(event){
+      ctx.fillStyle = 'rgb(255,255,255)';
+      ctx.fillRect(0,0,1280,640);
+      console.log(event.offsetX,event.offsetY);
+      ctx.drawImage(img,event.offsetX-100,event.offsetY-50,200,100,0,0,1280,640);
+    });
+
+    setInterval(function(){
       drawPixel(ctx,imageData);
-    },500);
+    },33);
   };
-  img.src = "img/castle.jpg"
+  img.src = "img/map.svg"
 };
 
 function drawPixel(ctx,data){
   for(let x = 10; x < data.width; x += 20){
-    setTimeout(function(){
-      ctx.fillStyle = 'rgb(255,255,255)';
-      ctx.fillRect(x-10,0,x+10,data.height);
+    //setTimeout(function(){},500);
 
-      for(let y = 10; y < data.height; y += 20){
-        ctx.beginPath();
-        ctx.fillStyle = getRGB(x,y,data);
-        // ctx.font = "19px Arial";
-        // ctx.textAlign = "center";
-        // ctx.fillText("A", x,y);
-        ctx.arc(x,y,10,0,2*Math.PI,true);
-        ctx.fill();
-      }
-    },1000);
+    ctx.fillStyle = 'rgb(255,255,255)';
+    ctx.fillRect(x-10,0,x+10,data.height);
+
+    for(let y = 10; y < data.height; y += 20){
+      ctx.beginPath();
+      ctx.fillStyle = getRGB(x,y,data);
+      ctx.arc(x,y,10,0,2*Math.PI,true);
+      ctx.fill();
+    }
   }
 }
 
